4c6bec149186cf8a877e88147b887740c6a73c6f
George Oprea <georgeoprea10@gmail.com>
2017-10-11 08:44:54 +0300

    Spring structure
    Added some spring structure

---
+++ Backend/ConfigHub/src/main/java/com/confighub/ConfigHub/ConfigHubApplication.java
@@ -0,0 +1,12 @@
+package com.confighub.ConfigHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigHubApplication.class, args);
+	}
+}
