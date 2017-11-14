bb45f6da8a75943f7c7837f876ba26c665f0ceb1
nadinast <nadistreanga@gmail.com>
2017-11-14 23:56:28 +0200

    Complete set of files for user db and spring backend

--- Backend/users/src/test/java/user/UsersApplicationTests.java
+++ Backend/users/src/test/java/user/UsersApplicationTests.java
@@ -1,4 +1,4 @@
-package com.confighub.ConfigHub;
+package user;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigHubApplicationTests {
+public class UsersApplicationTests {
 
 	@Test
 	public void contextLoads() {
